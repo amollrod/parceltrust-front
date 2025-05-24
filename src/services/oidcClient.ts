76631a3d55ecc77bbe2c996bb7ea9a81c504bdb3
amollrod <alex.moll.rodriguez@gmail.com>
2025-05-24 13:15:06 +0200

    redirect logout

--- src/services/oidcClient.ts
+++ src/services/oidcClient.ts
@@ -2,12 +2,12 @@ import { UserManager, WebStorageStateStore } from 'oidc-client-ts';
 
 const oidcClient = new UserManager({
     authority: "http://localhost:8082",
-    client_id: 'parceltrust-client',
+    client_id: "parceltrust-client",
     redirect_uri: "http://localhost:5173/callback",
     silent_redirect_uri: "http://localhost:5173/silent-renew",
-    response_type: 'code',
-    scope: 'openid',
-    post_logout_redirect_uri: "http://localhost:5173/",
+    response_type: "code",
+    scope: "openid",
+    post_logout_redirect_uri: "http://localhost:5173",
     automaticSilentRenew: true,
     userStore: new WebStorageStateStore({ store: window.localStorage }),
 });
